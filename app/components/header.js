5103e65fd7199b505b0f1b9c25466108cd9aba2f
HippieJim <hippiejim2896@gmail.com>
2025-01-06 08:58:37 -0600

    Add and style logo image in header

--- app/components/header.js
+++ app/components/header.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BioRhyme_Expanded } from "next/font/google";
+import Image from "next/image";
+import { BioRhyme } from "next/font/google";
 import Facebook from "./Facebook";
 import Insta from "./Insta";
 import Bandcamp from "./Bandcamp";
@@ -7,22 +8,30 @@ import Tiktok from "./Tiktok";
 import Youtube from "./Youtube";
 import Spotify from "./Spotify";
 
-const biorhyme = BioRhyme_Expanded({
+const biorhyme = BioRhyme({
   weight: "400",
   subsets: ["latin"],
 });
 
 const Header = () => {
   return (
-    <div className='absolute w-full h-24 flex justify-around text-white'>
+    <div className='absolute w-full flex justify-around text-white'>
       <div className={`flex items-center text-2xl ${biorhyme.className} mb-1`}>
-        <ul className='flex gap-5'>
-          <li className='nav'>Home</li>
+        <Image
+          className='opacity-70 hover:opacity-100 mr-8'
+          src='/logo.png'
+          alt='Jim Williams Logo'
+          width={150}
+          height={150}
+        />
+        <ul className='flex gap-4'>
           <li className='nav'>Bio</li>
           <li className='nav'>Music</li>
           <li className='nav'>Contact</li>
         </ul>
       </div>
+
+      <div className='h-[150px] w-[150px]'></div>
 
       <div className='flex gap-5 items-center'>
         <Facebook className='social' />
--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="h-full w-full bg-[url('/HomeBG.png')] bg-cover bg-no-repeat rounded-2xl">
+    <div className="h-full w-full bg-[url('/HomeBG.png')] bg-center bg-contain bg-no-repeat rounded-2xl">
       <div className='h-full w-full bg-black/30 p-8'></div>
     </div>
   );

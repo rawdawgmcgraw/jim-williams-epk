e6d9d9d322e8808dcd25c62ff6a2323983be2708
HippieJim <hippiejim2896@gmail.com>
2025-01-06 09:45:09 -0600

    Add web links to social icons

--- app/components/header.js
+++ app/components/header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { BioRhyme } from "next/font/google";
+import { BioRhyme_Expanded } from "next/font/google";
 import Facebook from "./Facebook";
 import Insta from "./Insta";
 import Bandcamp from "./Bandcamp";
@@ -8,7 +8,7 @@ import Tiktok from "./Tiktok";
 import Youtube from "./Youtube";
 import Spotify from "./Spotify";
 
-const biorhyme = BioRhyme({
+const biorhyme = BioRhyme_Expanded({
   weight: "400",
   subsets: ["latin"],
 });
@@ -17,14 +17,15 @@ const Header = () => {
   return (
     <div className='absolute w-full flex justify-around text-white'>
       <div className={`flex items-center text-2xl ${biorhyme.className} mb-1`}>
-        <Image
+        {/* <Image
           className='opacity-70 hover:opacity-100 mr-8'
           src='/logo.png'
           alt='Jim Williams Logo'
           width={150}
           height={150}
-        />
+        /> */}
         <ul className='flex gap-4'>
+          <li className='nav'>Home</li>
           <li className='nav'>Bio</li>
           <li className='nav'>Music</li>
           <li className='nav'>Contact</li>
@@ -34,12 +35,36 @@ const Header = () => {
       <div className='h-[150px] w-[150px]'></div>
 
       <div className='flex gap-5 items-center'>
-        <Facebook className='social' />
-        <Insta className='social' />
-        <Tiktok className='social' />
-        <Youtube className='social' />
-        <Bandcamp className='social' />
-        <Spotify className='social' />
+        <a
+          href='https://www.facebook.com/profile.php?id=61551916748821'
+          target='_blank'
+        >
+          <Facebook className='social' />
+        </a>
+        <a
+          href='https://www.instagram.com/thejimwilliamsmusic/'
+          target='_blank'
+        >
+          <Insta className='social' />
+        </a>
+        <a href='https://www.tiktok.com/@thejimwilliams' target='_blank'>
+          <Tiktok className='social' />
+        </a>
+        <a
+          href='https://www.youtube.com/channel/UCDY0enG0D2824vckC-Ri6iQ'
+          target='_blank'
+        >
+          <Youtube className='social' />
+        </a>
+        <a href='https://jimwilliamsmusic.bandcamp.com/' target='_blank'>
+          <Bandcamp className='social' />
+        </a>
+        <a
+          href='https://open.spotify.com/artist/3qyXnPjQ47e17GzCScQVc4'
+          target='_blank'
+        >
+          <Spotify className='social' />
+        </a>
       </div>
     </div>
   );
